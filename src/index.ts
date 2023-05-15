3186ec2062584f7d9a263a4c82c77685ed8c0178
Sid <84025779+methlox@users.noreply.github.com>
2023-05-16 02:21:31 +0530

    minor error in minting NFT

---
+++ src/index.ts
@@ -0,0 +1,116 @@
+import { initializeKeypair } from "./initializeKeypair"
+import * as web3 from "@solana/web3.js"
+import {
+  Metaplex,
+  keypairIdentity,
+  bundlrStorage,
+  toMetaplexFile,
+  NftWithToken,
+} from "@metaplex-foundation/js"
+import * as fs from "fs"
+
+const tokenName = "Sid"
+const description = "very unfunny"
+const symbol = "SID"
+const sellerFeeBasisPoints = 100
+const imageFile = "sid.jpeg"
+
+async function main() {
+  const connection = new web3.Connection(web3.clusterApiUrl("devnet"))
+  const user = await initializeKeypair(connection)
+
+  const metaplex = Metaplex.make(connection)
+    .use(keypairIdentity(user))
+    .use(
+      bundlrStorage({
+        address: "https://devnet.bundlr.network",
+        providerUrl: "https://api.devnet.solana.com",
+        timeout: 60000,
+      })
+    )
+
+  // file to buffer
+  const buffer = fs.readFileSync("src/" + imageFile)
+
+  // buffer to metaplex file
+  const file = toMetaplexFile(buffer, imageFile)
+
+  // upload image and get image uri
+  const imageUri = await metaplex.storage().upload(file)
+  console.log("image uri:", imageUri)
+
+  // upload metadata and get metadata uri (off chain metadata)
+  const { uri } = await metaplex
+    .nfts()
+    .uploadMetadata({
+      name: tokenName,
+      description: description,
+      image: imageUri,
+    })
+
+  console.log("metadata uri:", uri)
+
+  await createNft(metaplex, uri)
+
+   // You can get this from the Solana Explorer URL 
+   const mintAddress = new web3.PublicKey("EPd324PkQx53Cx2g2B9ZfxVmu6m6gyneMaoWTy2hk2bW")
+   await updateNft(metaplex, uri, mintAddress)
+}
+
+// create NFT
+async function createNft(
+  metaplex: Metaplex,
+  uri: string
+): Promise<NftWithToken> {
+  const { nft } = await metaplex
+    .nfts()
+    .create({
+      uri: uri,
+      name: tokenName,
+      sellerFeeBasisPoints: sellerFeeBasisPoints,
+      symbol: symbol,
+    },
+      // { commitment: "finalized" },
+    )
+
+  console.log(
+    `Token Mint: https://explorer.solana.com/address/${nft.address.toString()}?cluster=devnet`
+  )
+
+  return nft
+}
+
+// update NFT
+async function updateNft(
+  metaplex: Metaplex,
+  uri: string,
+  mintAddress: PublicKey
+) {
+  // get "NftWithToken" type from mint address
+  const nft = await metaplex.nfts().findByMint({ mintAddress })
+
+  // omit any fields to keep unchanged
+  await metaplex
+    .nfts()
+    .update({
+      nftOrSft: nft,
+      name: tokenName,
+      symbol: symbol,
+      uri: uri,
+      sellerFeeBasisPoints: sellerFeeBasisPoints,
+    })
+
+  console.log(
+    `Token Mint: https://explorer.solana.com/address/${nft.address.toString()}?cluster=devnet`
+  )
+}
+
+main()
+  .then(() => {
+    console.log("Finished successfully")
+    process.exit(0)
+  })
+  .catch((error) => {
+    console.log(error)
+    process.exit(1)
+  })
